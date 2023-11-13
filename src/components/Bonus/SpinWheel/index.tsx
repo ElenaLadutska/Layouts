d01e4caba75e8f9c1c97fd81ac9b2ce858dfcdff
Elena Ladutska <ladutskaelena@gmail.com>
2023-11-13 19:05:56 +0300

    added dots on the edges of the dropped cell for highlighting

--- src/components/Bonus/SpinWheel/index.tsx
+++ src/components/Bonus/SpinWheel/index.tsx
@@ -11,27 +11,33 @@ interface RotationValue {
   value: number;
   box: number;
   index: number;
+  rotation: string;
 }
 
 const rotationValues: RotationValue[] = [
-  { minDegree: 0, maxDegree: 25, value: 100, box: 1, index: 4 },
-  { minDegree: 340, maxDegree: 360, value: 100, box: 1, index: 4},
-  { minDegree: 26, maxDegree: 70, value: 50, box: 2, index: 2 },
-  { minDegree: 71, maxDegree: 115, value: 20, box: 1, index: 1 },
-  { minDegree: 116, maxDegree: 160, value: 50, box: 2, index: 4 },
-  { minDegree: 161, maxDegree: 205, value: 2, box: 1, index: 3 },
-  { minDegree: 206, maxDegree: 250, value: 5, box: 2, index: 1 },
-  { minDegree: 251, maxDegree: 295, value: 15, box: 1, index: 2  },
-  { minDegree: 296, maxDegree: 339, value: 2, box: 2, index: 3  },
+  { minDegree: 0, maxDegree: 25, value: 100, box: 1, index: 4, rotation: '0deg' },
+  { minDegree: 340, maxDegree: 360, value: 100, box: 1, index: 4, rotation: '0deg' },
+  { minDegree: 26, maxDegree: 70, value: 50, box: 2, index: 2, rotation: '-45deg' },
+  { minDegree: 71, maxDegree: 115, value: 20, box: 1, index: 1, rotation: '90deg' },
+  { minDegree: 116, maxDegree: 160, value: 50, box: 2, index: 4, rotation: '225deg' },
+  { minDegree: 161, maxDegree: 205, value: 2, box: 1, index: 3, rotation: '180deg' },
+  { minDegree: 206, maxDegree: 250, value: 5, box: 2, index: 1, rotation: '135deg' },
+  { minDegree: 251, maxDegree: 295, value: 15, box: 1, index: 2, rotation: '90deg' },
+  { minDegree: 296, maxDegree: 339, value: 2, box: 2, index: 3, rotation: '45deg' },
 ];
 
 const VALUES1: number[] = [20, 15, 2, 100];
 const VALUES2: number[] = [5, 50, 20, 50];
 
 const SpinWheel: React.FC = () => {
-  const wheelRef = useRef<HTMLDivElement>(null);
+  const wheelRef = useRef<HTMLDivElement>(null); 
+  const shadowRef = useRef<HTMLDivElement>(null); 
+  const leftDotRef = useRef<HTMLDivElement>(null); 
+  const rightDotRef = useRef<HTMLDivElement>(null); 
   const spinBtnRef = useRef<HTMLButtonElement>(null);
-  const [currentSpan, setCurrentSpan] = useState<{ box: number; index: number } | undefined>({box: 1, index: 4});
+  const [currentSpan, setCurrentSpan] = useState<
+    { box: number; index: number} | undefined
+    >({box: 1, index: 4});
   
   const handleRotationValue = (value: number) => {
     const matchingRotationValue = rotationValues.find(
@@ -39,9 +45,14 @@ const SpinWheel: React.FC = () => {
     );
 
     if (matchingRotationValue){
-      const {box, index} = matchingRotationValue;
-      
+      const {box, index, rotation} = matchingRotationValue;
       setCurrentSpan({box, index});
+
+      if (shadowRef.current && leftDotRef.current && rightDotRef.current) {
+        leftDotRef.current.style.display = 'block';
+        rightDotRef.current.style.display = 'block';
+        shadowRef.current.style.transform = `rotate(${rotation})`;
+      }
     }
   };
 
@@ -50,7 +61,7 @@ const SpinWheel: React.FC = () => {
 
     const spinBtnClickHandler = () => {
       setCurrentSpan({box: 0, index: 0});
-      
+
       if (spinBtnCurrent) {
         spinBtnCurrent.disabled = true;
       }
@@ -67,7 +78,7 @@ const SpinWheel: React.FC = () => {
 
             if (spinBtnCurrent) {
               spinBtnCurrent.disabled = false;
-            }
+            };
             
             break;
           }
@@ -83,6 +94,11 @@ const SpinWheel: React.FC = () => {
           wheelRef.current.style.transform = `rotate(${currentRotation}deg)`;
         }
 
+        if (leftDotRef.current && rightDotRef.current) {
+          leftDotRef.current.style.display = 'none';
+          rightDotRef.current.style.display = 'none';
+        }
+
         if (currentRotation >= 360) {
           count += 1;
           resultValue -= 5;
@@ -93,10 +109,8 @@ const SpinWheel: React.FC = () => {
 
           count = 0;
           resultValue = 101;
-        }
-
+        };
       };
-
 
       const rotationInterval = window.setInterval(rotateWheel, 10);
     };
@@ -119,17 +133,26 @@ const SpinWheel: React.FC = () => {
       <Arrow className={styles.arrow} />
 
       <div className={styles.wheelWrapper} ref={wheelRef}>
-        <div className={styles.shadow}></div>
+        <div ref={shadowRef} className={styles.shadow}>
+          <div ref={leftDotRef} className={styles.leftDot}></div>
+          <div ref={rightDotRef} className={styles.rightDot}></div>
+        </div>
 
         <div className={styles.box1}>
           {
             currentSpan?.box === 1 
             ? VALUES1.map((value, index) => (
-              <span key={index} className={`${styles[`span${[index + 1]}`]} ${currentSpan?.index === index+ 1 ? styles.active : ''}`}>
+              <span key={index} 
+                className={`${styles
+                  [`span${[index + 1]}`]} ${currentSpan?.index === index + 1 
+                    ? styles.active 
+                    : ''}`}>
+                
                 <b>{value}</b>
               </span>)) 
             : VALUES1.map((value, index) => (
                 <span key={index} className={styles[`span${[index + 1]}`]}>
+
                   <b>{value}</b>
                 </span>))
           }
@@ -139,11 +162,17 @@ const SpinWheel: React.FC = () => {
         {
             currentSpan?.box === 2 
             ? VALUES2.map((value, index) => (
-              <span key={index} className={`${styles[`span${[index + 1]}`]} ${currentSpan?.index === index + 1 ? styles.active : ''}`}>
+              <span key={index} 
+                className={`${styles[
+                  `span${[index + 1]}`]} ${currentSpan?.index === index + 1 
+                    ? styles.active 
+                    : ''}`}>
+
                 <b>{value}</b>
               </span>)) 
             : VALUES2.map((value, index) => (
                 <span key={index} className={styles[`span${[index + 1]}`]}>
+
                   <b>{value}</b>
                 </span>))
           }
